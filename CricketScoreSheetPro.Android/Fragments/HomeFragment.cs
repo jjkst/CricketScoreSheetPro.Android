719dcb08b5485d9627555d754ea0caf8c33735ab
karthik jayaraman <karthi.jayaraman@gmail.com>
2018-03-01 23:35:26 -0500

    build successful

---
+++ CricketScoreSheetPro.Android/Fragments/HomeFragment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CricketScoreSheetPro.Android.Fragments
+{
+    public class HomeFragment : Fragment
+    {
+        private Button mNewGameButton;
+        private Button mSavedGameButton;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.Inflate(Resource.Layout.HomeView, container, false);
+
+            mNewGameButton = rootView.FindViewById<Button>(Resource.Id.newgameButton);
+            mSavedGameButton = rootView.FindViewById<Button>(Resource.Id.savedgamesButton);
+
+            //mNewGameButton.Click += (object sender, EventArgs args) =>
+            //{
+            //    FragmentTransaction transaction = FragmentManager.BeginTransaction();
+            //    NewGameDialogFragment newGameDialog = new NewGameDialogFragment();
+            //    newGameDialog.SetStyle(DialogFragmentStyle.NoTitle, 0);
+            //    newGameDialog.Show(transaction, "newgame dialog");
+            //};
+
+            //mSavedGameButton.Click += (object sender, EventArgs args) =>
+            //{
+            //    var matchActivity = new Intent(this.Activity, typeof(MatchesActivity));
+            //    StartActivity(matchActivity);
+            //};
+
+            return rootView;
+        }
+    }
+}
---
+++ CricketScoreSheetPro.Android/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Content.PM;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using Android.Support.V7.Widget;
+using Android.Support.Design.Widget;
+using CricketScoreSheetPro.Android.Fragments;
+
+namespace CricketScoreSheetPro.Android
+{
+    [Activity(Label = "Cricket Score Sheet", Theme = "@style/MyTheme", MainLauncher = true, Icon = "@drawable/ic_launcher"
+        , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : AppCompatActivity
+    {
+        private ActionBarDrawerToggle _drawerToggle;
+        private DrawerLayout _drawerLayout;
+        private FragmentManager _fragmentManager;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            _fragmentManager = FragmentManager;
+            SetContentView (Resource.Layout.Main);
+
+            // Initialize toolbar
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetTitle(Resource.String.ApplicationName);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+
+            // Attach item selected handler to navigation view
+            var navigationView = FindViewById<NavigationView>(Resource.Id.left_drawer);
+            navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+            // Create ActionBarDrawerToggle button and add it to the toolbar
+            _drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);            
+            _drawerToggle = new ActionBarDrawerToggle(this, _drawerLayout, toolbar,
+                Resource.String.ApplicationName, Resource.String.ApplicationName);
+            _drawerLayout.AddDrawerListener(_drawerToggle);
+            _drawerToggle.SyncState();
+
+            var ft = _fragmentManager.BeginTransaction();
+            ft.Add(Resource.Id.content_frame, new HomeFragment());
+            ft.Commit();
+        }
+
+        private void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            var ft = FragmentManager.BeginTransaction();
+            switch (e.MenuItem.ItemId)
+            {
+                case (Resource.Id.nav_home):
+                    SupportActionBar.SetTitle(Resource.String.ApplicationName);
+                    ft.Replace(Resource.Id.content_frame, new HomeFragment());
+                    break;
+                //case (Resource.Id.nav_tournaments):
+                //    SupportActionBar.SetTitle(Resource.String.CompletedMatches);
+                //    ft.Replace(Resource.Id.content_frame, new MatchesFragment());
+                //    break;
+                //case (Resource.Id.nav_batsmanstats):
+                //    SupportActionBar.SetTitle(Resource.String.BatsmanStats);
+                //    ft.Replace(Resource.Id.content_frame, new BatsmanStatsFragment());
+                //    break;
+                //case (Resource.Id.nav_bowlerstats):
+                //    SupportActionBar.SetTitle(Resource.String.BowlerStats);
+                //    ft.Replace(Resource.Id.content_frame, new BowlerStatsFragment());
+                //    break;
+            }
+            ft.Commit();
+            _drawerLayout.CloseDrawers();
+        }
+    }
+}
+
